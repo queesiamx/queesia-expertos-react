763d518dd34a0a7a6ae22c970a650c067257cc89
queesiamx <queesiamx.employee@gmail.com>
2025-07-28 14:57:39 -0600

    ajustes de redes sociales

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
+// src/components/Footer.jsx
 import { Instagram, Facebook, Send } from "lucide-react";
+import { FaTiktok, FaXTwitter, FaYoutube } from "react-icons/fa6";
 
 export default function Footer() {
   return (
@@ -9,6 +11,7 @@ export default function Footer() {
         </p>
 
         <div className="flex gap-4 items-center justify-center">
+          {/* Instagram */}
           <a
             href="https://www.instagram.com/quees_ia"
             target="_blank"
@@ -18,6 +21,8 @@ export default function Footer() {
           >
             <Instagram className="w-5 h-5" />
           </a>
+
+          {/* Facebook */}
           <a
             href="https://www.facebook.com/share/16tCkmXBzp/"
             target="_blank"
@@ -27,14 +32,57 @@ export default function Footer() {
           >
             <Facebook className="w-5 h-5" />
           </a>
+
+          {/* Threads */}
+<a
+  href="https://www.threads.net/@quees_ia"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="hover:text-purple-400 transition"
+  title="Threads"
+>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 512 512"
+    fill="currentColor"
+    className="w-5 h-5"
+  >
+    <path d="M254.5 32C136.6 32 64 111.6 64 215.5c0 72.3 48.1 138.6 132.5 160.2 9.3-6.4 18.1-13.7 26.3-21.9-49.1-12.2-81.4-52.6-81.4-106.7 0-80.3 60.4-137.3 145.1-137.3 73.6 0 130.6 52.1 130.6 129.3 0 92.8-78.4 168.1-185.1 168.1-21.4 0-44.6-3.5-63.2-8.6-9.4 11.6-21.4 22.3-34.5 30.7 32.3 9.7 70.7 14.6 102.4 14.6 130.3 0 224.1-88.3 224.1-203.4C446.3 116.7 361.9 32 254.5 32z"/>
+  </svg>
+</a>
+
+
+          {/* TikTok */}
           <a
-            href="https://www.threads.net/@quees_ia"
+            href="https://www.tiktok.com/@quees_ia"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-purple-400 transition"
-            title="Threads"
+            className="hover:text-white transition"
+            title="TikTok"
           >
-            <Send className="w-5 h-5 rotate-45" />
+            <FaTiktok className="w-5 h-5" />
+          </a>
+
+          {/* X (Twitter) */}
+          <a
+            href="https://x.com/quees_ia"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-white transition"
+            title="X (Twitter)"
+          >
+            <FaXTwitter className="w-5 h-5" />
+          </a>
+
+          {/* YouTube */}
+          <a
+            href="https://www.youtube.com/@Quees_IA"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:text-red-500 transition"
+            title="YouTube"
+          >
+            <FaYoutube className="w-5 h-5" />
           </a>
         </div>
 
