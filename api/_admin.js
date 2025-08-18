fe1e737926965c39bdc1afb78bc285f4affc05b5
queesiamx <queesiamx.employee@gmail.com>
2025-08-17 20:03:11 -0600

    ajustes de stripe versión mejorada

--- api/_admin.js
+++ api/_admin.js
@@ -1,4 +1,4 @@
-// /api/_admin.js  (ESM; asegúrate de tener "type":"module" en package.json)
+// /api/_admin.js  (ESM)
 import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 
@@ -7,10 +7,7 @@ if (!getApps().length) {
     credential: cert({
       projectId: process.env.FB_PROJECT_ID,
       clientEmail: process.env.FB_CLIENT_EMAIL,
-      // Si pegaste la private key con \n escapados en el env de Vercel, esta línea es correcta:
       privateKey: process.env.FB_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-      // Si pegaste la key con saltos REALES (no \n), usa en su lugar:
-      // privateKey: process.env.FB_PRIVATE_KEY,
     }),
   });
 }
