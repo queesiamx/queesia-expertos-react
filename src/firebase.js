93d3d82ae30f160d155c27710ec59260c72127b8
queesiamx <queesiamx.employee@gmail.com>
2025-10-30 11:39:30 -0600

    Auth: usar expertos.queesia.com como authDomain

--- src/firebase.js
+++ src/firebase.js
@@ -14,7 +14,9 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN, // p.ej. queesia-e0de5.firebaseapp.com
+  //authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN, // p.ej. queesia-e0de5.firebaseapp.com
+ // Mismo dominio para evitar third-party en móviles
+  authDomain: "expertos.queesia.com",
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
